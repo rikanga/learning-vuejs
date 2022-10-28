0977628c0cebeba3f08a3e6a6696f960f997fb9c
rikanga <richardkangamba3@gmail.com>
2022-10-28 19:41:55 +0100

    HomeLink.vue first component created

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div id='app'>
+    <nav>
+      <HomeLink/>
+      <a href='/about'>A propos</a>
+      <a href='/contact'>Contact</a>
+    </nav>
+    <p>
+      Bienvenue sur notre page d'<HomeLink/>
+    </p>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HomeLink from './components/HomeLink.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    HomeLink,
   }
 }
 </script>
@@ -22,5 +35,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+a{
+  margin:5px;
 }
 </style>
---
+++ src/components/HomeLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id='app'>
+        <a href='/'>Accueil</a>
+    </div>
+
+</template>
+
+<script>
+export default{
+    name:'HomeLink'
+}
+</script>
+
+<style>
+a{
+    text-decoration:none;
+    display:inline;
+}
+</style>
