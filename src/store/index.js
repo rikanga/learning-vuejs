659090e7216966022f16ad48b18016c84bafed62
rikanga <richardkangamba3@gmail.com>
2022-11-03 22:25:00 +0100

    Vuex, state, getters, mutations and actions learned

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,46 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     // Ajout des données uniques de vérité
-    day:27,
-    month:4,
-    year:2022
+    day:25,
+    month:3,
+    year:2022,
+    count:0,
   },
   getters: {
+    formatteDate(state){
+      return `${state.day}/${state.month}/${state.year}`
+    }
   },
   mutations: {
+    INCREMENT_DAY(state){
+      state.day  += 1;
+    },
+    DECREMENT_DAY(state){
+      state.day -= 1;
+    },
+    INCREMENT_COUNT(state, payload=1){
+      state.count += Number(payload);
+    },
+    DECREASE_COUNT(state, payload=-1){
+      state.count -= Number(payload);
+    }
   },
   actions: {
+    increment_mont(context, amount){
+      context.commit('DECREMENT_MONTH', amount);
+    },
+    increment_count(context, number){
+      context.commit("INCREMENT_COUNT", number);
+    },
+    updateCount(context,  number){
+      if(number >= 0){
+        context.commit('INCREMENT_COUNT', number);
+      }else{
+        context.commit("DECREASE_COUNT", number);
+      }
+    }
   },
   modules: {
+    
   }
 })
--- src/views/NavView.vue
+++ src/views/NavView.vue
@@ -10,6 +10,30 @@
             Data from state manager of vuex: <br/>
             {{ $store.state.day }} / {{ $store.state.month }} / {{ $store.state.year }}
         </p>
+
+        <!-- Accès simplifier au state du store avec mapStore -->
+        <p>
+            Data from mapState
+            {{ day }} - {{ month }} - {{ year }}
+        </p>
+        <p>
+            Custom datas: 
+            {{ customDay }} - {{ uniqueMonth }} - {{ customYear }}
+        </p>
+
+        <p>
+            Using getters
+            {{ $store.getters.formatteDate }}
+        </p>
+        <button @click='increment_day'>Incrémenter</button>
+        <button @click='decrement_day'>Décrémenter</button>
+
+        <p>
+            Count {{ count }}
+            <button @click="increment_count"> Compter </button>
+            <button @click="decrement_count"> Décomptage </button>
+            <button @click="updateCount(-1)"> Update Count </button>
+        </p>
     </div>
 </template>
 
@@ -18,10 +42,37 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 import NavLink from '../components/NavLink.vue';
 
+import { mapState, mapActions } from 'vuex';
+
 export default {
     name:'NavView',
     components:{
         NavLink
+    },
+    computed:{
+        ...mapState(['day', 'month', 'year']),
+        // Customiser les noms de donneés
+        ...mapState({
+            customDay:'day',
+            uniqueMonth:'month',
+            customYear:'year',
+            count:'count'
+        })
+    },
+    methods:{
+        increment_day(){
+            this.$store.commit('INCREMENT_DAY')
+        },
+        decrement_day(){
+            this.$store.commit('DECREMENT_DAY');
+        },
+        increment_count(){
+            this.$store.dispatch('updateCount', 1);
+        },
+        decrement_count(){
+            this.$store.dispatch('updateCount', -1);
+        },
+        ...mapActions(['updateCount'])
     }
 }
 </script>
