f602202544675a9f02ce1738d8b7c28439665d29
rikanga <richardkangamba3@gmail.com>
2022-11-13 22:45:32 +0100

    Project initialized

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate';
+import auth from './modules/auth';
 
 export default createStore({
   state: {
@@ -10,5 +12,7 @@ export default createStore({
   actions: {
   },
   modules: {
-  }
+    auth
+  },
+  plugins:[createPersistedState()]
 })
